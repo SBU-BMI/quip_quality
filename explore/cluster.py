2b772c56c7bf22e5e089217b30d0cf8e2d91ea47
Jeremy Logan <jlogan7@utk.edu>
2017-08-09 10:10:57 -0400

    Simple k-means clustering with mugshots

--- explore/cluster.py
+++ explore/cluster.py
@@ -10,9 +10,15 @@ import sklearn.metrics as sm
 import numpy as np
 import pandas as pd
 import os
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+#Use interactive mode
+plt.ion()
 #import csv
+
+import matplotlib.image as mpimg
+import string
 
 rootdir = "/data/shared/jlogan/wensi20170523/luad"
 
@@ -39,35 +45,113 @@ frame = pd.DataFrame()
 flist = []
 
 for f in csvfiles:
-    #Let's use pandas instead of python to read csv...
     #with open(f) as featurefile:
         #csv.field_size_limit(2000000) # Large number to accomodate results with VERY large polygon descriptors
         #reader = csv.DictReader(featurefile)
-    df = pd.read_csv(f, index_col=None, header=0, usecols=range(92))
+
+
+    #Let's use pandas instead of python to read csv...
+    df = pd.read_csv(f, index_col=None, header=0, usecols=range(93))
     #df = pd.read_csv(f, index_col=None, header=0, usecols=[0,1,2,3])
     #df = pd.read_csv(f, index_col=None, header=0)
+    df['file'] = f
     flist.append(df)
-frame = pd.concat(flist)
+frame = pd.concat(flist, ignore_index=True)
 
 print "Loaded dataframe containing %i features:"%len(list(frame))
 print list(frame)
 
-frame.plot(kind='scatter', x='PhysicalSize', y='b_IntensityStd')
+tolerance = 5
+fig = plt.figure()
+ax = fig.add_subplot(121)
+
+#Plot the entire image
+#plotframe = frame
+#    -- OR --
+#Select a tile by index
+plotframe = flist[10]
+
+print plotframe.iloc[:,:91]
 
 # Let's try k-means with
-#model = KMeans (n_clusters=6)
-#model.fit (frame)
+model = KMeans (n_clusters=6)
+model.fit (plotframe.iloc[:,:91])
 
-#print model.labels_
+print model.labels_
 
 # Make a scatter plot
-#cm = np.array(['red','blue','green','orange','yellow','violet'])
+cm = np.array(['red','blue','green','orange','yellow','violet'])
 #df.plot(kind='scatter', x='PhysicalSize', y='b_IntensityStd', c=cm[model.labels_])
 
-# Output some mug shots
+
+plotframe.plot(ax=ax, kind='scatter', x='PhysicalSize', y='Circularity', picker=tolerance, c=cm[model.labels_])
+
+def getxy(f):
+  xp = f.find('_x')
+  yp = f.find('_y')
+  zp = f.find('-', yp)
+  x = float(f[xp+2:yp])
+  y = float(f[yp+2:zp])
+  return (x,y)
+
+def onpick(event):
+    the_file = plotframe['file'][event.ind].values[0]
+    print ":::", 
+    the_file = string.replace(the_file,"features.csv","seg-overlay.png")
+    #the_file = string.replace(the_file,"features.csv","seg.png")
+    im = mpimg.imread(the_file)
+
+    poly_pts = plotframe['Polygon'][event.ind].values[0][1:-1].split(':') # the [1:-1] is to strip the [ and ] from the ends of the string
+    # Need min and max of evens, and min and max of odds, then subtract
+    # the tile offsets from the filename
+    evens = range(0, len(poly_pts), 2)
+    odds = range(1, len(poly_pts), 2)
+
+    minx = 1000000000
+    maxx = 0
+    miny = 1000000000
+    maxy = 0  
+  
+    for i in evens:
+        me = int(float(poly_pts[i]))
+        if me > maxx:
+            maxx = me
+        if me < minx:
+            minx = me
+
+    for i in odds:
+        me = int(float(poly_pts[i]))
+        if me > maxy:
+            maxy = me
+        if me < miny:
+            miny = me
+
+    print "found", minx, miny, maxx, maxy
+
+    # Adjust to tile coordinates
+
+#    tilex = 45056
+#    tiley = 36864
+    tilex, tiley = getxy(the_file)
+
+    minx = minx - tilex
+    miny = miny - tiley
+    maxx = maxx - tilex
+    maxy = maxy - tiley
+
+    marginx = marginy = 0
+
+    #cropped_im = im[minx:maxx, miny:maxy]    
+    cropped_im = im[miny-marginy:maxy+marginy, minx-marginx:maxx+marginx]    
+
+    fig.add_subplot(122)
+    imgplot = plt.imshow(cropped_im)
+    plt.draw()
 
 
-plt.show(block=True)
+cid = fig.canvas.callbacks.connect('pick_event', onpick)
+
+plt.show()
 
 
 
